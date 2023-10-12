50612df5a875330e4eeefd3c9754ce6580b70a72
Oliver Teichmann, PRODYNA SE <oliver.teichmann@prodyna.com>
2023-10-12 18:11:04 +0200

    Delete shell script approach.

--- Challenge-01/CreateContainerWeb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create Web container
-az container create -n web -g $rg -e "API_URL=http://${sqlapi_ip}:8080" --image "${acr_name}.azurecr.io/hack/web:1.0" --ip-address public --ports 80 \
-  --registry-username "$acr_usr" --registry-password "$acr_pwd"
-export web_ip=$(az container show -n web -g "$rg" --query ipAddress.ip -o tsv)
-echo "Please connect your browser to http://${web_ip} to test the correct deployment"
--- Challenge-01/CreateDatabase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export sql_server_name=sqlserver$RANDOM
-export sql_db_name=mydb
-export sql_username=azure
-export sql_password=Microsoft123!
-az sql server create -n "$sql_server_name" -g "$rg" -l "$location" --admin-user "$sql_username" --admin-password "$sql_password"
-export sql_server_fqdn=$(az sql server show -n "$sql_server_name" -g "$rg" -o tsv --query fullyQualifiedDomainName)
-az sql db create -n "$sql_db_name" -s "$sql_server_name" -g "$rg" -e Basic -c 5 --no-wait
