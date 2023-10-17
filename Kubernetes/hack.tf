547533d1d34914f2dd7cd8a9971871c0a39c0219
Darko Križić <darko@krizic.net>
2023-10-17 19:45:54 +0200

    TODOs for 02

--- Kubernetes/hack.tf
+++ Kubernetes/hack.tf
@@ -2,11 +2,4 @@ locals {
   public_hostname = "hack.${local.ingress_ip}.traefik.me"
 }
 
-// Create namespace hack
-resource "kubernetes_namespace" "hack" {
-  metadata {
-    name = "hack"
-  }
-}
-
-
+// TODO: Create namespace "hack"
