3ec8831a2435356d5bb77fafbf06c77a8fee25ee
Darko Križić <darko@krizic.net>
2023-10-17 19:37:04 +0200

    Remove resource that needs to be done as challenge

--- Azure/instances.tf
+++ Azure/instances.tf
@@ -60,39 +60,3 @@ resource "azurerm_mssql_firewall_rule" "hack" {
 }
 
 // TODO Create another container group for the web app in the same resource group. Remember to define the API_URL environment variable. This should be the public IP address of the sqlapi container group and use port 8080.
-
-resource "azurerm_container_group" "hack_web" {
-  name                = "web"
-  resource_group_name = azurerm_resource_group.hack.name
-  location            = azurerm_resource_group.hack.location
-  os_type             = "Linux"
-  ip_address_type     = "Public"
-
-  image_registry_credential {
-    server   = azurerm_container_registry.hack.login_server
-    username = azurerm_container_registry.hack.admin_username
-    password = azurerm_container_registry.hack.admin_password
-  }
-
-  container {
-    name   = "web"
-    image  = "${azurerm_container_registry.hack.login_server}/hack/web:1.0"
-    cpu    = 0.25
-    memory = 0.5
-
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-
-    environment_variables = {
-      API_URL = "http://${azurerm_container_group.hack_sqlapi.ip_address}:8080"
-    }
-  }
-
-  exposed_port {
-    port = 80
-  }
-
-  depends_on = [time_sleep.wait_for_images]
-}
