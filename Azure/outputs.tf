362559ed7e8097aae7339f93024704d92817dd82
Oliver Teichmann, PRODYNA SE <oliver.teichmann@prodyna.com>
2023-10-17 18:38:33 +0200

    Revert implementation of challenge 02

--- Azure/outputs.tf
+++ Azure/outputs.tf
@@ -8,3 +8,13 @@ output "sql_server_password" {
   value     = azurerm_mssql_server.hack.administrator_login_password
   sensitive = true
 }
+
+// The URL to the web application
+output "web_container_url" {
+  value = "http://${azurerm_container_group.hack_web.ip_address}"
+}
+
+// The URL to the API
+output "api_container_url" {
+  value = "http://${azurerm_container_group.hack_web.ip_address}/api/healthcheck"
+}
